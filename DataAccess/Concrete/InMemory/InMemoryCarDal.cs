ccba07b39a5cf65f964cea96df3b743b3c3d5dd1
UmranGuzn <umran.guzn@hotmail.com>
2021-02-16 18:40:24 +0300

    30.01

---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,57 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car> {
+                new Car { CarId = 1, BrandId = 1, ColorId = 2, DailyPrice = 6000, Description = "kazalı", ModelYear = 2015 },
+                new Car { CarId = 2, BrandId = 2, ColorId = 1, DailyPrice = 7000, Description = "yeni", ModelYear = 2020},
+                new Car { CarId = 3, BrandId = 5, ColorId = 3, DailyPrice = 15000, Description = "öğretmenden", ModelYear = 2016 },
+                new Car { CarId = 4, BrandId = 4, ColorId = 4, DailyPrice = 16000, Description = "sıfır", ModelYear = 2017 }
+            };
+        }
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _cars.SingleOrDefault(c => c.CarId == car.CarId);
+
+            _cars.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public List<Car> GetAllByBrand(int brandId)
+        {
+            return _cars.Where(p => p.BrandId == brandId).ToList();
+        }
+
+        public List<Car> GetAllByColor(int colorId)
+        {
+            return _cars.Where(p => p.ColorId == colorId).ToList();
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c => c.CarId == car.CarId);
+
+            carToUpdate.Description = car.Description;
+            carToUpdate.DailyPrice = car.DailyPrice;
+        }
+    }
+}
