2b59502dcf9971fc571588c67e4d275f3952b793
UmranGuzn <umran.guzn@hotmail.com>
2021-02-17 21:09:52 +0300

    06.02 | Entities and DataAccess refactored

--- Business/Abstract/ICarService.cs
+++ Business/Abstract/ICarService.cs
@@ -1,4 +1,5 @@
 ﻿using Entities.Concrete;
+using Entities.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -11,6 +12,10 @@ namespace Business.Abstract
 
         List<Car> GetAllByBrandId(int id);
 
+        List<Car> GetAllByColorId(int id);
+
         List<Car> GetByDailyPrice(decimal min, decimal max);
+
+        List<CarDetailDto> GetCarDetails();
     }
 }
--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -1,6 +1,7 @@
 ﻿using Business.Abstract;
 using DataAccess.Abstract;
 using Entities.Concrete;
+using Entities.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -28,9 +29,19 @@ namespace Business.Concrete
             return _carDal.GetAll(p => p.BrandId == id);
         }
 
+        public List<Car> GetAllByColorId(int id)
+        {
+            return _carDal.GetAll(p => p.ColorId == id);
+        }
+
         public List<Car> GetByDailyPrice(decimal min, decimal max)
         {
             return _carDal.GetAll(p => p.DailyPrice >= min && p.DailyPrice <= max);
+        }
+
+        public List<CarDetailDto> GetCarDetails()
+        {
+            return _carDal.GetCarDetails();
         }
     }
 }
