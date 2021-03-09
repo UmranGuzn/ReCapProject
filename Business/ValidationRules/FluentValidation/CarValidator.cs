50e6465e5993991ef817e11fe966967598c8f7da
UmranGuzn <umran.guzn@hotmail.com>
2021-03-09 21:15:03 +0300

    FluentValidation Added

---
+++ Business/ValidationRules/FluentValidation/CarValidator.cs
@@ -0,0 +1,28 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+   public class CarValidator:AbstractValidator<Car>
+    {
+        public CarValidator()
+        {
+            RuleFor(p => p.Descriptions).NotEmpty();
+            RuleFor(p => p.Descriptions).MinimumLength(2);
+            RuleFor(p => p.DailyPrice).NotEmpty();
+            RuleFor(p => p.DailyPrice).GreaterThan(0);
+
+            RuleFor(p => p.DailyPrice).GreaterThanOrEqualTo(50000).When(p => p.BrandId == 1);
+
+            RuleFor(p => p.ModelYear).Must(StartWithTwo).WithMessage("Araçlar 2000 yılından önce olamaz");
+        }
+
+        private bool StartWithTwo(string arg)
+        {
+            return arg.StartsWith("2");
+        }
+    }
+}
