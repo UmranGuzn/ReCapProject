637d877374e84b95df47168871acd41dada0fb47
UmranGuzn <umran.guzn@hotmail.com>
2021-03-15 23:58:04 +0300

    SQLQuery Updated

---
+++ ReCapProjectSQLQuery.sql
@@ -0,0 +1,62 @@
+﻿CREATE TABLE [dbo].[Brands] (
+    [BrandId]   INT           IDENTITY (1, 1) NOT NULL,
+    [BrandName] NVARCHAR (50) NULL,
+    PRIMARY KEY CLUSTERED ([BrandId] ASC)
+);
+
+CREATE TABLE [dbo].[CarImages]
+(
+	[CarImageId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [CarId] INT NULL, 
+    [ImagePath] NVARCHAR(Max) NULL, 
+    [CarImageDate] DATETIME NULL,
+	FOREIGN KEY ([CarId]) REFERENCES [dbo].[Cars] ([CarId])
+);
+
+CREATE TABLE [dbo].[Cars] (
+    [CarId]        INT           IDENTITY (1, 1) NOT NULL,
+    [BrandId]      INT           NULL,
+    [ColorId]      INT           NULL,
+    [ModelYear]    NVARCHAR (25) NULL,
+    [DailyPrice]   DECIMAL (18)  NULL,
+    [Descriptions] NVARCHAR (25) NULL,
+    PRIMARY KEY CLUSTERED ([CarId] ASC),
+    FOREIGN KEY ([ColorId]) REFERENCES [dbo].[Colors] ([ColorId]),
+    FOREIGN KEY ([BrandId]) REFERENCES [dbo].[Brands] ([BrandId])
+);
+
+CREATE TABLE [dbo].[Colors] (
+    [ColorId]   INT           IDENTITY (1, 1) NOT NULL,
+    [ColorName] NVARCHAR (25) NULL,
+    PRIMARY KEY CLUSTERED ([ColorId] ASC)
+);
+
+CREATE TABLE [dbo].[Customers] (
+    [CustomerId]   INT           IDENTITY (1, 1) NOT NULL,
+    [UserId]       INT           NULL,
+    [CustomerName] NVARCHAR (30) NULL,
+    PRIMARY KEY CLUSTERED ([CustomerId] ASC),
+    FOREIGN KEY ([UserId]) REFERENCES [dbo].[Users] ([UserId])
+);
+
+
+CREATE TABLE [dbo].[Rentals] (
+    [RentalId]   INT      IDENTITY (1, 1) NOT NULL,
+    [CarId]      INT      NULL,
+    [CustomerId] INT      NULL,
+    [RentDate]   DATETIME NULL,
+    [ReturnDate] DATETIME NULL,
+    PRIMARY KEY CLUSTERED ([RentalId] ASC),
+    FOREIGN KEY ([CarId]) REFERENCES [dbo].[Cars] ([CarId]),
+    FOREIGN KEY ([CustomerId]) REFERENCES [dbo].[Customers] ([CustomerId])
+);
+
+CREATE TABLE [dbo].[Users] (
+    [UserId]    INT           IDENTITY (1, 1) NOT NULL,
+    [FirstName] NVARCHAR (25) NULL,
+    [LastName]  NVARCHAR (25) NULL,
+    [Email]     NVARCHAR (50) NULL,
+    [Password]  NVARCHAR (35) NULL,
+    PRIMARY KEY CLUSTERED ([UserId] ASC)
+);
+
